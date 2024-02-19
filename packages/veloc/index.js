51299cf8e088fa26c14ce79e584287a0298247b0
Yorn Qiu <yornqiu@gmail.com>
2024-02-19 17:04:29 +0800

    refactor: 修改导出方式

--- packages/veloc/index.js
+++ packages/veloc/index.js
@@ -2,14 +2,15 @@
  * @Author: Yorn Qiu
  * @Date: 2024-01-26 17:14:39
  * @LastEditors: Yorn Qiu
- * @LastEditTime: 2024-02-19 11:46:28
+ * @LastEditTime: 2024-02-19 17:01:34
  * @FilePath: /veloc/packages/veloc/index.js
  * @Description:
  */
 
-exports.default = require('./dist/core')
-exports.router = require('./dist/router')
-exports.static = require('./dist/static')
-exports.body = require('./dist/body')
-exports.cors = require('./dist/cors')
-exports.send = require('./dist/send')
+exports = module.exports = require('@veloc/core')
+exports.Router = require('@veloc/router')
+exports.Body = require('@veloc/body')
+exports.Cors = require('@veloc/cors')
+exports.Cookie = require('@veloc/cookie')
+exports.Static = require('@veloc/static')
+exports.Send = require('@veloc/send')
