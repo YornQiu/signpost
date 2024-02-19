e05f819046ed1818124df9c238fce6ea2661ef40
Yorn Qiu <yornqiu@gmail.com>
2024-02-19 11:54:29 +0800

    feat: package veloc

--- packages/veloc/index.js
+++ packages/veloc/index.js
@@ -2,14 +2,14 @@
  * @Author: Yorn Qiu
  * @Date: 2024-01-26 17:14:39
  * @LastEditors: Yorn Qiu
- * @LastEditTime: 2024-01-26 17:23:34
+ * @LastEditTime: 2024-02-19 11:46:28
  * @FilePath: /veloc/packages/veloc/index.js
  * @Description:
  */
 
-module.exports = require('@veloc/core')
-exports.router = require('@veloc/router')
-exports.static = require('@veloc/static')
-exports.body = require('@veloc/body')
-exports.cors = require('@veloc/cors')
-exports.send = require('@veloc/send')
+exports.default = require('./dist/core')
+exports.router = require('./dist/router')
+exports.static = require('./dist/static')
+exports.body = require('./dist/body')
+exports.cors = require('./dist/cors')
+exports.send = require('./dist/send')
